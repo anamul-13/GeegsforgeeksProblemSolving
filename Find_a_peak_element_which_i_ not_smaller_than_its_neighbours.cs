f707afb0e1d0bc3542c40724825569c5900de6f4
Anamul-Haque <anamul.cse.pust@gmail.com>
2023-02-26 15:40:31 +0600

    Find a peak element which is not smaller than its neighbours

---
+++ Find_a_peak_element_which_i_ not_smaller_than_its_neighbours.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter Array Length:");
+int n=int.Parse(Console.ReadLine());
+int[] array = new int[n];
+
+Console.WriteLine("Enter Array Index: ");
+for(int i=0; i<n; i++)
+{
+    array[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("The peak Element is: ");
+
+for(int i=1; i<array.Length-1; i++)
+{
+    if (array[i] > array[i-1] && array[i] > array[i+1])
+    {
+        Console.WriteLine(array[i]);
+    }
+}
